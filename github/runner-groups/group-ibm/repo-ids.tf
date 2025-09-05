57119f6ba12ec5bd055a632966e7e54e3de4ad74
andy-neuma <andy@neuralmagic.com>
2025-09-05 14:04:40 -0400

    simple runner group setup
    Signed-off-by: andy-neuma <andy@neuralmagic.com>

---
+++ github/runner-groups/group-ibm/repo-ids.tf
@@ -0,0 +1,7 @@
+# REPOs supported by group "ibm-runners"
+
+# If you are adding support for a new REPO, then please add its unique id.
+
+variable "vllm_id" {
+    default = 599547518
+}
---
+++ github/runner-groups/group-neural-magic/group.tf
@@ -0,0 +1,15 @@
+resource "github_actions_runner_group" "neural_magic_runners" {
+
+  name = "neural-magic-runners"
+
+  restricted_to_workflows    = false
+  allows_public_repositories = true
+  visibility                 = "selected"
+
+  selected_repository_ids = [
+    var.llm-compressor_id,
+    var.speculators_id,
+    var.vllm_id
+  ]
+
+}
