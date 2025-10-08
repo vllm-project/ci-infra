35c3da77199ebf4ad1896f4836a859d68c819b70
Alexei-V-Ivanov-AMD <156011006+Alexei-V-Ivanov-AMD@users.noreply.github.com>
2025-10-08 14:49:10 -0500

    Update bootstrap.sh
     Signed-off-by: Alexei V. Ivanov <alexei.ivanov@amd.com>

--- buildkite/bootstrap.sh
+++ buildkite/bootstrap.sh
@@ -76,7 +76,7 @@ upload_pipeline() {
     (
         set -x
         # Output pipeline.yaml with all blank lines removed
-        minijinja-cli test-template.j2 test-pipeline.yaml \
+        minijinja-cli test-template.j2 test-amd.yaml \
             -D branch="$BUILDKITE_BRANCH" \
             -D list_file_diff="$LIST_FILE_DIFF" \
             -D run_all="$RUN_ALL" \
