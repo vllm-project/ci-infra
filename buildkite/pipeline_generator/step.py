3f134090fa678c13696f6b0a39785627e599f6e7
Kevin H. Luu <khluu000@gmail.com>
2025-11-30 23:11:03 -0800

    add retry env
    Signed-off-by: Kevin H. Luu <khluu000@gmail.com>

--- buildkite/pipeline_generator/step.py
+++ buildkite/pipeline_generator/step.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Optional, List, Dict
+from typing import Optional, List, Dict, Any
 from pydantic import model_validator
 from typing_extensions import Self
 from collections import defaultdict
