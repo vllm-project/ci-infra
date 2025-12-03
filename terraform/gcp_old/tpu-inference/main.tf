c55ec0eb380e9cf8bd7d0e3622c9dd624ec05b6e
StingLin <sting.lin@cienet.com>
2025-12-03 15:22:23 +0800

    Recreate TPU v6e-1 agents to use ci_v6e module
    Signed-off-by: StingLin <sting.lin@cienet.com>

--- terraform/gcp_old/tpu-inference/main.tf
+++ terraform/gcp_old/tpu-inference/main.tf
@@ -18,14 +18,19 @@ data "google_secret_manager_secret_version" "huggingface_token" {
   version = "latest"
 }
 
-module "ci_v6" {
-  source = "./modules/ci_v6"
+module "ci_v6e_1" {
+  source    = "./modules/ci_v6e"
   providers = {
-    google-beta.us-east5-b = google-beta.us-east5-b
+    google-beta = google-beta.us-east5-b
   }
-  project_id              = var.project_id
-  buildkite_token_value   = data.google_secret_manager_secret_version.buildkite_agent_token_ci_cluster.secret_data
-  huggingface_token_value = data.google_secret_manager_secret_version.huggingface_token.secret_data
+
+  accelerator_type                 = "v6e-1"
+  reserved                         = false
+  instance_count                   = 24
+  buildkite_queue_name             = "tpu_v6e_queue"
+  project_id                       = var.project_id
+  buildkite_token_value            = data.google_secret_manager_secret_version.buildkite_agent_token_ci_cluster.secret_data
+  huggingface_token_value          = data.google_secret_manager_secret_version.huggingface_token.secret_data
 }
 
 module "ci_v6e_8" {
