96daa83741b97d206059d7e89b4254662691d52e
Kevin H. Luu <khluu000@gmail.com>
2025-12-03 00:37:45 -0800

    conditional testing
    Signed-off-by: Kevin H. Luu <khluu000@gmail.com>

--- buildkite/pipeline_generator/buildkite_step.py
+++ buildkite/pipeline_generator/buildkite_step.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Dict, List, Optional, Any, Union
+from typing import Dict, List, Optional, Any, Union, TypedDict
 from step import Step
 from utils import get_agent_queue, get_image
 from global_config import get_global_config
