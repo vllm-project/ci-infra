7fd4689dabbafff2427a8841e79a186f5b3dbd10
Kevin H. Luu <khluu000@gmail.com>
2025-12-04 13:35:37 -0800

    misc
    Signed-off-by: Kevin H. Luu <khluu000@gmail.com>

--- buildkite/pipeline_generator/buildkite_step.py
+++ buildkite/pipeline_generator/buildkite_step.py
@@ -71,7 +71,7 @@ def convert_group_step_to_buildkite_step(group_steps: Dict[str, List[Step]]) -> 
         for step in steps:
             # generate block step if step should not run automatically
             block_step = None
-            if not step_should_run(step, list_file_diff):
+            if 1==1 or not step_should_run(step, list_file_diff):
                 block_step = BuildkiteBlockStep(
                     block=f"Run {step.label}",
                     depends_on=[],
