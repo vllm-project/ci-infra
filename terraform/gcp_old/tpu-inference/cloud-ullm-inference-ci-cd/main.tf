328a3b544e28ca2be3e512c102fa1967f31dd05e
Qiliang Cui <derrhein@gmail.com>
2025-12-09 07:06:30 +0000

    Add tpuv7x machines.
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

--- terraform/gcp_old/tpu-inference/cloud-ullm-inference-ci-cd/main.tf
+++ terraform/gcp_old/tpu-inference/cloud-ullm-inference-ci-cd/main.tf
@@ -39,3 +39,20 @@ module "ci_v6e_8" {
   buildkite_token_value            = data.google_secret_manager_secret_version.buildkite_agent_token_ci_cluster.secret_data
   huggingface_token_value          = data.google_secret_manager_secret_version.huggingface_token.secret_data
 }
+
+module "ci_v7x_2" {
+  source    = "../modules/ci_v7x"
+  providers = {
+    google-beta = google-beta.us-central1-b
+  }
+
+  accelerator_type                 = "tpu7x-2"
+  reserved                         = true
+  instance_count                   = 1
+  buildkite_queue_name             = "tpu_v7x_2_queue"
+  disk_size                        = 1024
+  project_id                       = var.project_id
+  project_short_name               = var.project_short_name
+  buildkite_token_value            = data.google_secret_manager_secret_version.buildkite_agent_token_ci_cluster.secret_data
+  huggingface_token_value          = data.google_secret_manager_secret_version.huggingface_token.secret_data
+}
---
+++ terraform/gcp_old/tpu-inference/modules/ci_v7x/variables.tf
@@ -0,0 +1,45 @@
+variable "accelerator_type" {
+  type        = string
+  description = "Accelerator type of TPU"
+}
+
+variable "reserved" {
+  description = "if use reserved tpu resource"
+  type        = bool
+  default     = true
+}
+
+variable "instance_count" {
+  type        = number
+  description = "Number of TPU instance"
+}
+
+variable "disk_size" {
+  type        = number
+  description = "The large disk size"
+}
+
+variable "buildkite_queue_name" {
+  type        = string
+  description = "The Buildkite agent queue name that the agents will join."
+}
+
+variable "project_id" {
+  type        = string
+  description = "The project ID for creating TPU agents"
+}
+
+variable "project_short_name" {
+  type        = string
+  description = "Short name for improved readability"
+}
+
+variable "buildkite_token_value" {
+  type        = string
+  description = "Agent token used to connect to Buildkite."
+}
+
+variable "huggingface_token_value" {
+  type        = string
+  description = "Hugging Face token for vLLM model serving usage."
+}
