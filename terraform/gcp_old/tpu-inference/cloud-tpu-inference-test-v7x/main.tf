0711e76c3ecd8a1f67dd693d49944dfad04deb55
QiliangCui <derrhein@gmail.com>
2025-12-09 11:02:32 -0800

    Create new v7 project and state. (#240)
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

--- terraform/gcp_old/tpu-inference/cloud-tpu-inference-test-v7x/main.tf
+++ terraform/gcp_old/tpu-inference/cloud-tpu-inference-test-v7x/main.tf
@@ -1,40 +1,41 @@
 data "google_secret_manager_secret_version" "buildkite_agent_token_ci_cluster" {
-  secret = "projects/${var.secret_project_id}/secrets/tpu_commons_buildkite_agent_token"
+  secret = "projects/${var.project_id}/secrets/tpu_commons_buildkite_agent_token"
   version = "latest"
 }
 
 data "google_secret_manager_secret_version" "huggingface_token" {
-  secret  = "projects/${var.secret_project_id}/secrets/tpu_commons_buildkite_hf_token"
+  secret  = "projects/${var.project_id}/secrets/tpu_commons_buildkite_hf_token"
   version = "latest"
 }
 
-module "ci_v6e_1" {
-  source    = "../modules/ci_v6e"
+module "ci_v7x_2" {
+  source    = "../modules/ci_v7x"
   providers = {
-    google-beta = google-beta.us-central1-b
+    google-beta = google-beta.us-central1-c
   }
 
-  accelerator_type                 = "v6e-1"
+  accelerator_type                 = "tpu7x-2"
   reserved                         = true
-  instance_count                   = 18
+  instance_count                   = 4
+  buildkite_queue_name             = "tpu_v7x_2_queue"
   disk_size                        = 512
-  buildkite_queue_name             = "tpu_v6e_queue"
   project_id                       = var.project_id
   project_short_name               = var.project_short_name
   buildkite_token_value            = data.google_secret_manager_secret_version.buildkite_agent_token_ci_cluster.secret_data
   huggingface_token_value          = data.google_secret_manager_secret_version.huggingface_token.secret_data
 }
 
-module "ci_v6e_8" {
-  source    = "../modules/ci_v6e"
+module "ci_v7x_8" {
+  source    = "../modules/ci_v7x"
   providers = {
-    google-beta = google-beta.us-central1-b
+    google-beta = google-beta.us-central1-c
   }
 
-  accelerator_type                 = "v6e-8"
+  accelerator_type                 = "tpu7x-8"
   reserved                         = true
-  instance_count                   = 1
-  buildkite_queue_name             = "tpu_v6e_8_queue"
+  instance_count                   = 0
+  buildkite_queue_name             = "tpu_v7x_8_queue"
+  disk_size                        = 1024
   project_id                       = var.project_id
   project_short_name               = var.project_short_name
   buildkite_token_value            = data.google_secret_manager_secret_version.buildkite_agent_token_ci_cluster.secret_data
