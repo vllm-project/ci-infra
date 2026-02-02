e5b35dbbab1ec157f2b2999b5c89291bc547b88c
khluu <khluu000@gmail.com>
2026-02-02 15:22:56 -0800

    p
    Signed-off-by: khluu <khluu000@gmail.com>

--- buildkite/pipeline_generator/step.py
+++ buildkite/pipeline_generator/step.py
@@ -26,7 +26,7 @@ class Step(BaseModel):
     retry: Optional[Dict[str, Any]] = None
     optional: Optional[bool] = False
     no_plugin: Optional[bool] = False
-    mirror: Optional[Dict[str, str]] = None
+    mirror: Optional[Dict[str, Any]] = None
 
     @model_validator(mode="after")
     def validate_multi_node(self) -> Self:
