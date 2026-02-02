80fafd1119bc233a0f4746df04a5936c9dd9e851
khluu <khluu000@gmail.com>
2026-02-02 10:24:12 -0800

    p
    Signed-off-by: khluu <khluu000@gmail.com>

--- buildkite/pipeline_generator/buildkite_step.py
+++ buildkite/pipeline_generator/buildkite_step.py
@@ -301,6 +301,7 @@ def _create_amd_mirror_step(step: Step, original_commands: List[str], amd_queue:
     return BuildkiteCommandStep(
         label=amd_label,
         commands=[amd_command],
+        group="AMD",
         depends_on=["image-build-amd"],
         agents={"queue": amd_queue},
         env={"DOCKER_BUILDKIT": "1"},
