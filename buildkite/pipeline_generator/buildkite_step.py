47e1aa95ef9f671df09a72702e1db2bddaba5d27
khluu <khluu000@gmail.com>
2026-02-11 23:31:30 -0800

    p
    Signed-off-by: khluu <khluu000@gmail.com>

--- buildkite/pipeline_generator/buildkite_step.py
+++ buildkite/pipeline_generator/buildkite_step.py
@@ -252,6 +252,7 @@ def convert_group_step_to_buildkite_step(
 
 
 def _step_should_run(step: Step, list_file_diff: List[str]) -> bool:
+    return False
     global_config = get_global_config()
     if step.key and step.key.startswith("image-build"):
         return True
