05e862c3b48f8c8dc4c9266eb0b2bd829998487d
khluu <khluu000@gmail.com>
2026-02-24 16:59:16 -0800

    p
    Signed-off-by: khluu <khluu000@gmail.com>

--- buildkite/pipeline_generator/buildkite_step.py
+++ buildkite/pipeline_generator/buildkite_step.py
@@ -199,6 +199,7 @@ def convert_group_step_to_buildkite_step(
                 depends_on=step.depends_on,
                 soft_fail=step.soft_fail,
                 agents={"queue": get_agent_queue(step)},
+                priority=100,
             )
 
             if block_step:
