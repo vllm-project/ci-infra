c4e799d08df6a0f0be4ecf88054c2ccaac50dc2f
Qiliang Cui <derrhein@gmail.com>
2026-03-02 10:38:43 -0800

    [TPU] Update machine counts in each data center (#294)
    Signed-off-by: Qiliang Cui <derrhein@gmail.com>

--- terraform/gcp_old/tpu-inference/cloud-tpu-inference-test/main.tf
+++ terraform/gcp_old/tpu-inference/cloud-tpu-inference-test/main.tf
@@ -31,7 +31,7 @@ module "ci_v6e_1" {
 
   accelerator_type                 = "v6e-1"
   reserved                         = false
-  instance_count                   = 24
+  instance_count                   = 0
   buildkite_queue_name             = "tpu_v6e_queue"
   project_id                       = var.project_id
   project_short_name               = var.project_short_name
