fa45b65ea5972f4f728175993b08318716db44ee
khluu <khluu000@gmail.com>
2026-04-02 03:50:56 -0700

    Remove dead CPU gpus cleanup code

--- buildkite/pipeline_generator/plugin/docker_plugin.py
+++ buildkite/pipeline_generator/plugin/docker_plugin.py
@@ -71,7 +71,5 @@ def get_docker_plugin(step: Step, image: str):
 
     if step.label == "Benchmarks" or step.mount_buildkite_agent:
         plugin["mount_buildkite_agent"] = True
-    if step.device in (DeviceType.CPU, DeviceType.CPU_SMALL, DeviceType.CPU_MEDIUM) and plugin.get("gpus"):
-        del plugin["gpus"]
     # TODO: Add BUILDKITE_ANALYTICS_TOKEN and pytest addopts for fail_fast
     return plugin
