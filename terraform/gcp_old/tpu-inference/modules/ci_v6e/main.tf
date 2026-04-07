342f0c0f60f5527c7066c33a6d4d201f76a6ed44
Yiwei Wang <yiwei.dddd@gmail.com>
2026-04-07 21:12:10 +0000

    Force stop the buildkite-agent and start at the end of startup process
    Signed-off-by: Yiwei Wang <yiwei.dddd@gmail.com>

--- terraform/gcp_old/tpu-inference/modules/ci_v6e/main.tf
+++ terraform/gcp_old/tpu-inference/modules/ci_v6e/main.tf
@@ -58,6 +58,9 @@ resource "google_tpu_v2_vm" "tpu_v6_ci" {
       echo "deb [signed-by=/usr/share/keyrings/buildkite-agent-archive-keyring.gpg] https://apt.buildkite.com/buildkite-agent stable main" | sudo tee /etc/apt/sources.list.d/buildkite-agent.list
       apt-get update
       apt-get install -y buildkite-agent
+     
+      # Force stop the buildkite-agent and start at the end to avoid race condition
+      sudo systemctl stop buildkite-agent
 
       sudo usermod -a -G docker buildkite-agent
       sudo -u buildkite-agent gcloud auth configure-docker us-central1-docker.pkg.dev --quiet
