6fa99e006d7e01d46415c595eba895d0f85a6637
andy-neuma <andy@neuralmagic.com>
2026-04-28 11:35:32 -0400

    follow naming conventions
    Signed-off-by: andy-neuma <andy@neuralmagic.com>

--- buildkite/pipeline_generator/plugin/docker_plugin.py
+++ buildkite/pipeline_generator/plugin/docker_plugin.py
@@ -80,7 +80,7 @@ b200_plugin_template = {
     ],
 }
 
-zen5_plugin_template = {
+amd_zen5_plugin_template = {
     "image": "",
     "always-pull": True,
     "propagate-environment": True,
@@ -106,8 +106,8 @@ def get_docker_plugin(step: Step, image: str):
         plugin = copy.deepcopy(h200_plugin_template)
     elif step.device == DeviceType.B200:
         plugin = copy.deepcopy(b200_plugin_template)
-    elif step.device == DeviceType.ZEN5:
-        plugin = copy.deepcopy(zen5_plugin_template)
+    elif step.device == DeviceType.AMD_ZEN5_CPU:
+        plugin = copy.deepcopy(amd_zen5_plugin_template)
     else:
         plugin = copy.deepcopy(docker_plugin_template)
     plugin["image"] = image
