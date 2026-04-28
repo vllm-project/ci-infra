90f0fa7f863a2e160348cee82fce221abd541a2d
patrick ji <patrickji2014@gmail.com>
2026-04-28 17:44:42 +0000

    set the host_name env var so it can be used in kernel tuning jobs
    Signed-off-by: patrick ji <patrickji2014@gmail.com>

--- terraform/gcp_old/tpu-inference/modules/ci_v6e/main.tf
+++ terraform/gcp_old/tpu-inference/modules/ci_v6e/main.tf
@@ -66,11 +66,12 @@ resource "google_tpu_v2_vm" "tpu_v6_ci" {
       sudo -u buildkite-agent gcloud auth configure-docker us-central1-docker.pkg.dev --quiet
 
       sudo sed -i "s/xxx/${var.buildkite_token_value}/g" /etc/buildkite-agent/buildkite-agent.cfg
+      
       HOST_NAME_VAL="${var.accelerator_type}-ci-${count.index}-${var.project_short_name}-${data.google_client_config.config.zone}"
-      # Set the system-wide environment variable
+      # Set the system-wide environment variable and make it take effect
       echo "HOST_NAME=$HOST_NAME_VAL" | sudo tee -a /etc/environment
-      # Use the same local variable for the sed command
-      sudo sed -i "s/name=\"%hostname-%spawn\"/name=\"$HOST_NAME_VAL\"/" /etc/buildkite-agent/buildkite-agent.cfg
+      set -a; source /etc/environment; set +a
+      sudo sed -i "s/name=\"%hostname-%spawn\"/name=\"$HOST_NAME\"/" /etc/buildkite-agent/buildkite-agent.cfg
 
       echo 'tags="queue=${var.buildkite_queue_name}"' | sudo tee -a /etc/buildkite-agent/buildkite-agent.cfg
       echo 'HF_TOKEN=${var.huggingface_token_value}' | sudo tee -a /etc/environment
