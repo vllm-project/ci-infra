d213c39e1014a203a66a9cd2ec5a7df987583f10
andy-neuma <andy@neuralmagic.com>
2026-05-12 10:11:49 -0400

    Red Hat WDC H100s

--- buildkite/pipeline_generator/buildkite_step.py
+++ buildkite/pipeline_generator/buildkite_step.py
@@ -90,9 +90,9 @@ def get_agent_queue(step: Step):
     elif step.device == DeviceType.A100:
         return AgentQueue.A100
     elif step.device == DeviceType.H100:
-        # Route multi-GPU H100 tests to RedHat Frankfurt queue
+        # Route multi-GPU H100 tests to RedHat WDC queue
         if step.num_devices is not None and step.num_devices >= 4:
-            return AgentQueue.MITHRIL_H100
+            return AgentQueue.REDHAT_H100_WDC
         else:
             return AgentQueue.MITHRIL_H100
     elif step.device == DeviceType.H200:
--- buildkite/pipeline_generator/constants.py
+++ buildkite/pipeline_generator/constants.py
@@ -78,4 +78,4 @@ class AgentQueue(str, Enum):
     AMD_MI355_4 = "amd_mi355_4"
     AMD_MI355_8 = "amd_mi355_8"
     DGX_SPARK = "dgx-spark"
-    REDHAT_H100_FRANKFURT = "RedHat-H100-Frankfurt"
+    REDHAT_H100_WDC = "RedHat-H100-WDC"
