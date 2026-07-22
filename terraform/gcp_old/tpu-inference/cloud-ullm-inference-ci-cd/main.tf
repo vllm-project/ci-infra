89b2f4d54afde881b37583b0f3bcb33fc8d9e4d1
Ming Huang <mhhua@google.com>
2026-07-22 10:20:36 -0700

    Move some v7x2 to v7x8
    Signed-off-by: Ming Huang <mhhua@google.com>

--- terraform/gcp_old/tpu-inference/cloud-ullm-inference-ci-cd/main.tf
+++ terraform/gcp_old/tpu-inference/cloud-ullm-inference-ci-cd/main.tf
@@ -58,7 +58,7 @@ module "ci_v7x_2" {
 
   accelerator_type                = "tpu7x-2"
   reserved                        = true
-  instance_count                  = 16
+  instance_count                  = 12
   buildkite_queue_name            = "tpu_v7x_2_queue"
   disk_size                       = 2048
   project_id                      = var.project_id
@@ -76,7 +76,7 @@ module "ci_v7x_8" {
 
   accelerator_type                = "tpu7x-8"
   reserved                        = true
-  instance_count                  = 15
+  instance_count                  = 16
   buildkite_queue_name            = "tpu_v7x_8_queue"
   disk_size                       = 4096
   project_id                      = var.project_id
