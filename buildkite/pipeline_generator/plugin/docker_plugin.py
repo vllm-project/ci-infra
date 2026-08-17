b6ac6e0a9f0aa135ec57b6c209b14c1d0fb873cb
khluu <khluu000@gmail.com>
2026-08-17 04:00:48 -0700

    Revert "Enable command and pytest tracing centrally (#472)"
    This reverts commit 8a3c992f73861616e36b9ce4d707dce796fd6b0c.
    
    Signed-off-by: khluu <khluu000@gmail.com>

--- buildkite/pipeline_generator/plugin/docker_plugin.py
+++ buildkite/pipeline_generator/plugin/docker_plugin.py
@@ -141,11 +141,7 @@ def get_docker_plugin(step: Step, image: str):
     if step.device in (DeviceType.H200_18GB, DeviceType.H200_35GB):
         image = image.replace("public.ecr.aws", "936637512419.dkr.ecr.us-west-2.amazonaws.com/vllm-ci-pull-through-cache")
         plugin["image"] = image
-    if (
-        step.label == "Benchmarks"
-        or step.mount_buildkite_agent
-        or step.otel_tracing_enabled()
-    ):
+    if step.label == "Benchmarks" or step.mount_buildkite_agent:
         plugin["mount_buildkite_agent"] = True
     if step.device in (DeviceType.CPU, DeviceType.CPU_SMALL, DeviceType.CPU_MEDIUM) and plugin.get("gpus"):
         del plugin["gpus"]
